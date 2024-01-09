1bb140ebbc9e1cd41b5a238514ba492bd7484b98
Simon Donike <accounts@donike.net>
2024-01-09 15:59:58 +0000

    dependencies

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Super-Resolute whole Sen2 tile
-Check instructions in  .ipynb.
+# opensr-utils - Supplementary Code for the ESA OpenSR project
+This package contains useful functions to perform super-resolution of raw Sentinel-2 tiles. More detailed documentation coming soon.
--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
 from setuptools import setup, find_packages
 
+# read the contents of README file
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='opensr-utils',
     version='0.1.2',
+    authors = [
+      { name="Simon Donike", email="author@example.com" },
+      { name="Cesar Aybar"},
+      { name="Luis Gomez Chova"},
+      { name="Freddie Kalaitzis"},
+        ],
+    description = "Utils supporting the ESA openst-model package for Super-Resolution of Senintel-2 Imagery",
+    Homepage = "https://isp.uv.es/opensr/"
     packages=find_packages(),
-    install_requires=[],
-    # Add more fields as needed
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+	'numpy',
+	'einops',
+	'rasterio'
+	'tqdm',
+	'torch',
+	'scikit-image'],
 )
