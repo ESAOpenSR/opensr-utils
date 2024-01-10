90244df92730978b5cfccb42ce832bb1474e565c
Simon Donike <accounts@donike.net>
2024-01-10 13:27:00 +0000

    v6

---
+++ opensr_utils/__init__.py
@@ -0,0 +1,6 @@
+# local imports
+from denormalize_image_per_band_batch import denormalize_image_per_band_batch as denorm
+from stretching import hq_histogram_matching
+
+from bands10m_stacked_from_S2_folder import extract_10mbands_from_S2_folder
+from bands20m_stacked_from_S2_folder import extract_20mbands_from_S2_folder
--- opensr_utils/main.py
+++ opensr_utils/main.py
@@ -8,8 +8,10 @@ import os
 
 
 # local imports
-from denormalize_image_per_band_batch import denormalize_image_per_band_batch as denorm
-from stretching import hq_histogram_matching
+#from denormalize_image_per_band_batch import denormalize_image_per_band_batch as denorm
+#from stretching import hq_histogram_matching
+#from utils.bands10m_stacked_from_S2_folder import extract_10mbands_from_S2_folder
+#from utils.bands20m_stacked_from_S2_folder import extract_20mbands_from_S2_folder
 
 
 class windowed_SR_and_saving():
@@ -318,7 +320,6 @@ class windowed_SR_and_saving():
             # if it doesnt exist, create stacked LR images
             if not os.path.exists(os.path.join(self.folder_path,"stacked_10m.tif")):
                 print("Stacked 10m bands not found, creating ...")
-                from utils.bands10m_stacked_from_S2_folder import extract_10mbands_from_S2_folder
                 extract_10mbands_from_S2_folder(self.folder_path)
 
             # Get File information - 10m bands
@@ -343,7 +344,6 @@ class windowed_SR_and_saving():
             # if it doesnt exist, create stacked LR images
             if not os.path.exists(os.path.join(self.folder_path,"stacked_20m.tif")):
                 print("Stacked 20m bands not found, creating ...")
-                from utils.bands20m_stacked_from_S2_folder import extract_20mbands_from_S2_folder
                 extract_20mbands_from_S2_folder(self.folder_path)
 
             # Get File information - 20m bands 
