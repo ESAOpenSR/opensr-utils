420f9695ff2765f6563d328e42c766e7f307717a
Simon Donike <accounts@donike.net>
2024-05-10 14:55:10 +0100

    xAI randomization fix, relax requirements for dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='opensr-utils',
-    version='0.4.25',
+    version='0.4.26',
     author = "Simon Donike, Cesar Aybar, Luis Gomez Chova, Freddie Kalaitzis",
     author_email = "accounts@donike.net",
     description = "Utilities supporting the ESA opensr-model package for Super-Resolution of Senintel-2 Imagery",
@@ -17,12 +17,12 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
-	'numpy==1.23.5',
-	'einops==0.6.0',
-	'rasterio==1.3.6',
-	'tqdm==4.64.1',
-	'torch==1.13.1',
-	'scikit-image==0.19.3',
-        'torchvision==0.14.1',
-        'pytorch-lightning==1.9.0'],
+	'numpy',
+	'einops',
+	'rasterio',
+	'tqdm',
+	'torch',
+	'scikit-image',
+        'torchvision',
+        'pytorch-lightning'],
 )
