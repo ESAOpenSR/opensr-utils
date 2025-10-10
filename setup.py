91ad9c3b3edae7c21667bf7111beb85f79e9b9c3
Simon Donike <accounts@donike.net>
2025-10-10 13:32:23 +0100

    damn imports

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='opensr-utils',
-    version='1.1.1',
+    version='1.1.2',
     author = "Simon Donike, Cesar Aybar, Luis Gomez Chova, Freddie Kalaitzis",
     author_email = "accounts@donike.net",
     description = "Utilities supporting the ESA opensr-model package for Super-Resolution of Senintel-2 Imagery",
     url = "https://opensr.eu/",
     project_urls={'Source Code': 'https://github.com/ESAopenSR/opensr-utils'},
     license='MIT',
-    packages=find_packages(),
+    packages=find_packages(include=["opensr_utils", "opensr_utils.*"]),
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
